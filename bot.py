5801022efb847b46c63d3ace5e294798c730307f
Yujiao Mo <moyujiao0515@gmail.com>
2018-03-30 09:58:07 +0800

    Added a function send map for loc qn

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
-from adam_qas.qas.adam import main as adam_main, init_qas 
-from neuralcoref.neuralcoref import Coref
+import sys
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
-from chatterbot import ChatBot
+import telegram
+from telegram import KeyboardButton
+from loc import get_lat_lng
+from questionparser import QuestionParser
 
-chatbot = ChatBot(
-    'Ron Obvious',
-    trainer = 'chatterbot.trainers.ChatterBotCorpusTrainer'
-)
+# from chatterbot import ChatBot
+# chatbot = ChatBot(
+#     'Ron Obvious',
+#     trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+# )
 
-chatbot.train("chatterbot.corpus.english")
-coref = Coref()
-qas = init_qas()
+# chatbot.train("chatterbot.corpus.english")
+
+# initiate bot
 try:
     with open('token.txt', 'r') as f:
         TOKEN = f.readline().strip()
@@ -18,26 +21,42 @@ except IOError:
     print("No token file. Please create a token.txt with your token in the first line.")
     sys.exit()
 
-
+# command function
+# command: /start
+# output: greeting message
 def start(bot, update):
     sender = update.message.chat.first_name
-    greeting = "Hi, " + sender + "! How can I help you? Use /info to ask me factoid questions, please!"
+    help_msg = "! How can I help you? Use /info to ask me factoid questions, please!"
+    greeting = "Hi, " + sender + help_msg
     bot.send_message(chat_id=update.message.chat_id, text=greeting)
 
-def courtesy_reply(bot, update):
-    message = update.message.text
-    reply = chatbot.get_response(message)
-    print(reply)
-    update.message.reply_text(reply.text)
+# message function
+# output: converse with user
+# def courtesy_reply(bot, update):
+#     message = update.message.text
+#     reply = chatbot.get_response(message)
+#     print(reply)
+#     update.message.reply_text(reply.text)
 
+# command & message function
+# command: /info
+# message: a question
+# output: question parsing result
 def information_reply(bot, update):
     message = update.message.text
-    bot.send_message(chat_id=update.message.chat_id, text="(Only replying question type and keywords as of now)")
-    coref.continuous_coref(utterances = message)
-    question = coref.get_resolved_utterances()
+    # status_msg = "(Only replying question type and keywords as of now)"
+    # bot.send_message(chat_id=update.message.chat_id, text=status_msg)
+    question = " ".join(message.split()[1:])
     print(question)
-    answer = adam_main(qas, question)
-    update.message.reply_text(answer)
+    qp = QuestionParser(question)
+    qp.preprocess()
+    e_loc = qp.get_loc_entity()
+    print(qp.get_type())
+    if e_loc and qp.get_type() == 'LOC':
+        cood = get_lat_lng(" ".join(e_loc))
+        bot.sendLocation(chat_id=update.message.chat_id, latitude=cood[0], longitude=cood[1])
+    else: update.message.reply_text(qp.get_type())
+    
 
 def error_handler(error):
     print("Caught an error", error)
@@ -48,15 +67,12 @@ def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
     dispatcher.add_error_handler(error_handler)
-    chatty = MessageHandler(Filters.text, courtesy_reply)
-    dispatcher.add_handler(chatty)
+    # chatty = MessageHandler(Filters.text, courtesy_reply)
+    # dispatcher.add_handler(chatty)
     info_handler = CommandHandler('info', information_reply)
     dispatcher.add_handler(info_handler)
-    
     updater.start_polling()
-
     updater.idle()
 
 if __name__ == '__main__':
     main()
-
--- loc.py
+++ loc.py
@@ -6,27 +6,9 @@ try:
 except IOError:
     print("No token file. Please create a token.txt with your token in the first line.")
     sys.exit()
+
 def get_lat_lng(add):
     response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}".format(add, API))
     location = response.json()["results"][0]["geometry"]["location"]
     return [location['lat'], location['lng']]
 
-# checks if the question asks for location
-# and if there is "ORGANIZATION" entity
-# returns the location needs to be searched for
-def isLocation(q_class, entity):
-    orgs = []
-    if q_class == "['LOC']" and entity:
-        for e in entity:
-            if e[1] in LOC:
-                orgs.append(e[0])
-    return orgs
-
-# uses http request to ask Google Map about the location
-# returns latitude and longitude of the location
-def sendGMap(orgs):
-    location = []
-    for o in orgs:
-        title = " ".join(o.split(","))
-        location.append(get_lat_lng(title))
-    return location
